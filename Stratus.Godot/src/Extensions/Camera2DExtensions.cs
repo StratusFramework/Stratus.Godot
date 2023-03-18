16070fb28a2f5c5c5dea3382bbb71f2998f47db7
Christian Sagel <ckpsm@live.com>
2023-03-18 18:11:16 +0100

    Add the tilemap node 2d

---
+++ Stratus.Godot/src/Extensions/Camera2DExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Godot;
+
+namespace Stratus.Godot.Extensions
+{
+    public static class Camera2DExtensions
+    {
+		public static void Limit(this Camera2D camera, TileMap map)
+		{
+			var mapRect = map.GetUsedRect();
+			var tileSize = map.CellQuadrantSize;
+			var worldSizeInPixels = mapRect.Size * tileSize;
+			camera.LimitLeft = 0;
+			camera.LimitTop = 0;
+			camera.LimitRight = worldSizeInPixels.X;
+			camera.LimitBottom = worldSizeInPixels.Y;
+		}
+		
+		public static string LimitsToString(this Camera2D camera)
+		{
+			return $"X({camera.LimitLeft}, {camera.LimitRight}), Y({camera.LimitBottom}, {camera.LimitTop})";
+		}
+	}
+}
---
+++ Stratus.Godot/src/TileMapNode2D.cs
@@ -0,0 +1,39 @@
+﻿using Godot;
+
+namespace Stratus.Godot
+{
+	/// <summary>
+	/// A node that works on top of an existing <see cref="TileMap"/>
+	/// </summary>
+	public partial class TileMapNode2D : Node2D
+	{
+		public TileMap tileMap { get; private set; }
+
+		public void Initialize(TileMap tileMap)
+		{
+			this.tileMap = tileMap;
+		}
+
+		public void Move(Vector2I direction)
+		{
+			var cellPosition = tileMap.LocalToMap(Position);
+			cellPosition += direction;
+			Snap(cellPosition);
+		}
+
+		public void Snap(Vector2I position)
+		{
+			if (!Contains(position))
+			{
+				return;
+			}
+			var localPos = tileMap.MapToLocal(position);
+			Position = localPos;
+		}
+
+		public bool Contains(Vector2I position)
+		{
+			return tileMap.GetCellTileData(0, position) != null;
+		}
+	}
+}
