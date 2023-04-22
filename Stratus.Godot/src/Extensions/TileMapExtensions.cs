bc318da453350418f7572609cfecfa52c6387cd9
Christian Sagel <ckpsm@live.com>
2023-04-22 20:38:56 +0200

    Add extension to instantiate a scene onto a tilemap

---
+++ Stratus.Godot/src/Extensions/TileMapExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Godot;
+
+namespace Stratus.Godot.Extensions
+{
+	public static class TileMapExtensions
+	{
+		public static TNode? InstantiateScene<TNode>(this TileMap tileMap, PackedScene scene, Vector2I pos)
+			where TNode : Node2D
+		{
+			var instance = scene.Instantiate<TNode>();
+			tileMap.AddChild(instance);
+			var localPos = tileMap.MapToLocal(pos);
+			instance.Position = localPos;
+			return instance;
+		}
+	}
+
+}
