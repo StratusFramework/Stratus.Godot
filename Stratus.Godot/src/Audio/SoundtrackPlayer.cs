436386a9c2b62e851d3fbad91f370c5db5928014
Christian Sagel <ckpsm@live.com>
2023-09-16 15:29:04 +0200

    Add a player for the soundtrack

---
+++ Stratus.Godot/src/Audio/SoundtrackPlayer.cs
@@ -0,0 +1,27 @@
+﻿using Godot;
+
+using Stratus.Godot.Extensions;
+using Stratus.Models.Audio;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratus.Godot.Audio
+{
+	public partial class SoundtrackPlayer : AudioStreamPlayer
+	{
+		public override void _Ready()
+		{
+			base._Ready();
+			GodotEventSystem.Connect<PlayAudioEvent>(OnPlayAudioEvent);
+		}
+
+		private void OnPlayAudioEvent(PlayAudioEvent e)
+		{
+			this.Log($"Will play {e.name}");
+		}
+	}
+}
